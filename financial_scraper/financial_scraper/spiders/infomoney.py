5090fef0b5fedca5fbf198cbf7a1637baf843643
LuisaSantello <luisasantello@usp.br>
2025-08-06 20:54:21 -0300

    mudanca

--- financial_scraper/financial_scraper/spiders/infomoney.py
+++ financial_scraper/financial_scraper/spiders/infomoney.py
@@ -4,10 +4,11 @@ from financial_scraper.items import FinancialNewsItem
 class InfoMoneySpider(scrapy.Spider):
     name = "infomoney"
     allowed_domains = ["infomoney.com.br"]
-    start_urls = ["https://www.infomoney.com.br/ultimas-noticias/"]
+    #start_urls = ["https://www.infomoney.com.br/ultimas-noticias/"]
+    start_urls = ["https://www.infomoney.com.br/mercados/"]
 
     def parse(self, response):
-        for card in response.css('div.hfeed article'):
+        for card in response.css('a.hover:underline::attr(href)'):
             link = card.css('a::attr(href)').get()
             if link:
                 yield response.follow(link, self.parse_article)
@@ -17,6 +18,6 @@ class InfoMoneySpider(scrapy.Spider):
         item['title'] = response.css('h1::text').get()
         item['url'] = response.url
         item['date'] = response.css('time::attr(datetime)').get()
-        item['content'] = " ".join(response.css('div.entry-content p::text').getall())
+        item['content'] = " ".join(response.css('div.text-lg md:text-xl font-medium tracking-tight text-wl-neutral-600" p::text').getall())
         item['source'] = "InfoMoney"
         yield item
